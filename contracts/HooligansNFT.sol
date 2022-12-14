8e35ae34a2da643c689c1c031a6f5bec6b3d785f
zvchvry <zach@hooliganstribe.net>
2022-12-13 23:48:24 -0500

    ready??

--- contracts/HooligansNFT.sol
+++ contracts/HooligansNFT.sol
@@ -3,11 +3,10 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Hooligans is ERC721, ERC721Enumerable, Pausable, Ownable {
+contract Hooligans is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
 
     uint256 public maxSupply = 420;
@@ -25,14 +24,6 @@ contract Hooligans is ERC721, ERC721Enumerable, Pausable, Ownable {
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://bafybeiey6x74pi3xbbldac3kcqwy7ozpat3aufmruwfw5j33s6mow6nsca/";
-    }
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
     }
 
     function editMintWindows(
@@ -77,7 +68,6 @@ function withdraw(address _addr) external onlyOwner{
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
-        whenNotPaused
         override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
